7c894df748b8a6b8fbe79323f4921039bd80f76f
Max Erebus <max.erebus@gmail.com>
2020-01-03 13:59:40 +0400

    Now you can pass a key for a object to sorting algorithms

--- Examples/Node/test.js
+++ Examples/Node/test.js
@@ -4,7 +4,40 @@ console.log(rainLib);
 
 const testArray = rainLib.generation.randomArrayNumbers(10, -100, 100);
 
-console.log('\nStarting Test Array:', testArray, '\n\n');
+const testArrayOfObjects = [
+    {
+        movie: 'example one',
+        description: 'lorem ipsum',
+        rating: 5.5,
+    },
+    {
+        movie: 'example one',
+        description: 'lorem ipsum',
+        rating: 1.9,
+    },
+    {
+        movie: 'example one',
+        description: 'lorem ipsum',
+        rating: 7,
+    },
+    {
+        movie: 'example one',
+        description: 'lorem ipsum',
+        rating: 4.2,
+    },
+    {
+        movie: 'example one',
+        description: 'lorem ipsum',
+        rating: 5.3,
+    },
+    {
+        movie: 'example one',
+        description: 'lorem ipsum',
+        rating: 5.5,
+    },
+]
+
+console.log('\nStarting Test Array Of Numbers:', testArray, '\n\n');
 
 console.log('Selection Sort:', rainLib.sort.selection(testArray, true));
 console.log('Test Array:', testArray, '\n');
@@ -14,3 +47,14 @@ console.log('Test Array:', testArray, '\n');
 
 console.log('Recursive Bubble Sort:', rainLib.sort.recursiveBubble(testArray, true));
 console.log('Test Array:', testArray);
+
+console.log('\nStarting Test Array Of Objects:', testArrayOfObjects, '\n\n');
+
+console.log('Bubble Sort With Key:', rainLib.sort.bubble(testArrayOfObjects, true, "rating"));
+console.log('Test Array:', testArrayOfObjects, '\n');
+
+console.log('Recursive Bubble Sort With Key:', rainLib.sort.recursiveBubble(testArrayOfObjects, true, "rating"));
+console.log('Test Array:', testArrayOfObjects);
+
+console.log('Selection Sort With Key:', rainLib.sort.selection(testArrayOfObjects, true, "rating"));
+console.log('Test Array:', testArrayOfObjects, '\n');
